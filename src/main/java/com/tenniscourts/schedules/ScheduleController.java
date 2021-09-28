3ba33bf52783ea0eb54e49f8cfd9e4c88de175b8
Timmy Kyaw <39270802+tripletk@users.noreply.github.com>
2021-09-28 08:12:01 -0700

    Implemented ScheduleController routes

--- src/main/java/com/tenniscourts/schedules/ScheduleController.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleController.java
@@ -1,8 +1,22 @@
 package com.tenniscourts.schedules;
 
 import com.tenniscourts.config.BaseRestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,23 +24,41 @@ import java.time.LocalTime;
 import java.util.List;
 
 @AllArgsConstructor
+@RestController
+@RequestMapping(value = "/schedules")
 public class ScheduleController extends BaseRestController {
 
-    private final ScheduleService scheduleService;
+    private final ScheduleService scheduleService = null;
 
     //TODO: implement rest and swagger
-    public ResponseEntity<Void> addScheduleTennisCourt(CreateScheduleRequestDTO createScheduleRequestDTO) {
+    @ApiOperation(value = "Add to Tennis Court Schedule")
+	@ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 400, message = "An unexpected error occurred")
+    })
+	@PostMapping
+    public ResponseEntity<Void> addScheduleTennisCourt(@RequestBody CreateScheduleRequestDTO createScheduleRequestDTO) {
         return ResponseEntity.created(locationByEntity(scheduleService.addSchedule(createScheduleRequestDTO.getTennisCourtId(), createScheduleRequestDTO).getId())).build();
     }
 
-    //TODO: implement rest and swagger
-    public ResponseEntity<List<ScheduleDTO>> findSchedulesByDates(LocalDate startDate,
-                                                                  LocalDate endDate) {
+    @ApiOperation(value = "Find schedules by Date")
+	@ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 400, message = "An unexpected error occurred")
+    })
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ScheduleDTO>> findSchedulesByDates(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate)  {
         return ResponseEntity.ok(scheduleService.findSchedulesByDates(LocalDateTime.of(startDate, LocalTime.of(0, 0)), LocalDateTime.of(endDate, LocalTime.of(23, 59))));
     }
 
-    //TODO: implement rest and swagger
-    public ResponseEntity<ScheduleDTO> findByScheduleId(Long scheduleId) {
+    @ApiOperation(value = "Find schedule by Id")
+	@ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 400, message = "An unexpected error occurred")
+    })
+    @GetMapping(path="/{scheduleId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ScheduleDTO> findByScheduleId(@PathVariable Long scheduleId) {
         return ResponseEntity.ok(scheduleService.findSchedule(scheduleId));
     }
 }
